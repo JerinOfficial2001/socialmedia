05db107b210e2303da11755bba5154aa3ec44511
jerin2001 <jerinjerijree@gmail.com>
2023-04-10 17:29:48 +0530

    metadata

--- pages/adminpage/admin_components/Comments.js
+++ pages/adminpage/admin_components/Comments.js
@@ -10,7 +10,7 @@ import { useUser } from "@supabase/auth-helpers-react/dist";
 
 function Comments() {
   const user =useUser()
- const username=user.user_metadata.first_name
+//  const username=user.user_metadata.first_name
   return (
     <Box
       flex={1}
@@ -70,7 +70,7 @@ function Comments() {
               <Typography
                 sx={{ color: "black", fontSize: 10, fontWeight: "bold" }}
               >
-                {username}
+                {/* {username} */}Name
               </Typography>
               <IconButton size="small">
                 <ExpandMoreOutlinedIcon sx={{ fontSize: 16 }} />
