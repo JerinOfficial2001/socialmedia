8ec2365ef57a8330e97e7b425e610ef85726493c
jerin2001 <jerinjerijree@gmail.com>
2023-04-12 17:37:05 +0530

    rectified name at auth

--- components/Auth.js
+++ components/Auth.js
@@ -6,24 +6,22 @@ import Container from "@mui/material/Container";
 import { useUser } from "@supabase/auth-helpers-react";
 
 export default function Auth() {
-const user=useUser()
-
+  const user = useUser();
 
   const [validator, setvalidator] = useState(false);
   const [sucessMessage, setsucessMessage] = useState(false);
   const [invalidMessage, setinvalidMessage] = useState(false);
   const [signUpSwitch, setsignUpSwitch] = useState(false);
 
-
   const [inputDatas, setinputDatas] = useState({
     email: "",
     password: "",
-   
-    avatar_url:''
+
+    avatar_url: "",
   });
-  const { email, password,avatar_url } = inputDatas;
+  const { email, password, avatar_url } = inputDatas;
   const handleSubmit = async () => {
-    if ( email !== "" && password !== "") {
+    if (email !== "" && password !== "") {
       setvalidator(false);
       setinvalidMessage(false);
 
@@ -44,10 +42,10 @@ const user=useUser()
     console.log(inputDatas);
     setinputDatas({ email: "", password: "" });
   };
-  
+
   //signup
   const handleSubmitSignup = async () => {
-    if (email !== "" && password !== "" ) {
+    if (email !== "" && password !== "") {
       setvalidator(false);
       setinvalidMessage(false);
 
@@ -55,11 +53,6 @@ const user=useUser()
       const { error } = await supabase.auth.signUp({
         email: email,
         password: password,
-        options: {
-          data: {
-            first_name: name,
-           
-          }}
       });
       setsucessMessage(true);
       if (error) {
@@ -72,15 +65,12 @@ const user=useUser()
       setsucessMessage(false);
     }
     console.log(inputDatas);
-    setinputDatas({ email: "", password: "",avatar_url:''});
- 
+    setinputDatas({ email: "", password: "", avatar_url: "" });
   };
- 
 
   return (
-   
     <>
-    <Container
+      <Container
         sx={{
           display: "flex",
           flexDirection: "column",
@@ -89,131 +79,138 @@ const user=useUser()
           height: "100vh",
         }}
       >
-      {!signUpSwitch ? (
-        <>
-          <form className="login">
-          <Typography color="black" sx={{fontWeight:'bold'}}>LOG IN</Typography>
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => {
-                setinputDatas({ ...inputDatas, email: e.target.value });
-              }}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => {
-                setinputDatas({ ...inputDatas, password: e.target.value });
-              }}
-            />
-            {validator && (
-              <Typography color="red">All fields are mandatory*</Typography>
-            )}
-            {sucessMessage && (
-              <Typography color="green">Login success</Typography>
-            )}
-            {invalidMessage && (
-              <Typography color="red">Email or password incorrect!</Typography>
-            )}
-            <button
-           style={{background:'black'}}
-              onClick={(e) => {
-                handleSubmit(e);
-                e.preventDefault();
-              }}
-            >
-              Login
-            </button>
-            <Stack
-              direction="row"
-              spacing={1}
-              alignItems="center"
-              sx={{ marginTop: 2 }}
-            >
-              <Typography color="red">{"Don't have an account?"}</Typography>
-              <div
-                onClick={() => {
-                  setsignUpSwitch(true);
-                  setvalidator(false);
-                  setsucessMessage(false);
-                  setinvalidMessage(false);
-                }}
-                style={{ cursor: "pointer", color: "blue" }}
-              >
-                {"Sign up"}
-              </div>
-            </Stack>
-          </form>
-        </>
-      ) : (
-        <>
-          {/* signup */}
-       
-          <form className="login">
-          <Typography color="black" sx={{fontWeight:'bold'}}>SIGN UP</Typography>
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => {
-                setinputDatas({ ...inputDatas, email: e.target.value });
-              }}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => {
-                setinputDatas({ ...inputDatas, password: e.target.value });
-              }}
-            />
-            {validator && (
-              <Typography color="red">All fields are mandatory*</Typography>
-            )}
-            {sucessMessage && (
-              <Typography color="green">
-                We have sent a link to your email!
+        {!signUpSwitch ? (
+          <>
+            <form className="login">
+              <Typography color="black" sx={{ fontWeight: "bold" }}>
+                LOG IN
               </Typography>
-            )}
-            {invalidMessage && (
-              <Typography color="red">Give Strong Password!</Typography>
-            )}
-            <button
-            style={{background:'black'}}
-              onClick={(e) => {
-                handleSubmitSignup(e);
-                e.preventDefault();
-              }}
-            >
-              Sign Up
-            </button>
-            <Stack
-              direction="row"
-              spacing={1}
-              alignItems="center"
-              sx={{ marginTop: 2 }}
-            >
-              <Typography color="red">{"Already have an account?"}</Typography>
-              <div
-                onClick={() => {
-                  setsignUpSwitch(false);
-                  setvalidator(false);
-                  setsucessMessage(false);
-                  setinvalidMessage(false);
-                 
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => {
+                  setinputDatas({ ...inputDatas, email: e.target.value });
                 }}
-                style={{ cursor: "pointer", color: "blue" }}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => {
+                  setinputDatas({ ...inputDatas, password: e.target.value });
+                }}
+              />
+              {validator && (
+                <Typography color="red">All fields are mandatory*</Typography>
+              )}
+              {sucessMessage && (
+                <Typography color="green">Login success</Typography>
+              )}
+              {invalidMessage && (
+                <Typography color="red">
+                  Email or password incorrect!
+                </Typography>
+              )}
+              <button
+                style={{ background: "black" }}
+                onClick={(e) => {
+                  handleSubmit(e);
+                  e.preventDefault();
+                }}
               >
-                {"Log in"}
-              </div>
-            </Stack>
-          </form>
-        </>
-      )}
-   </Container>
-   </>
+                Login
+              </button>
+              <Stack
+                direction="row"
+                spacing={1}
+                alignItems="center"
+                sx={{ marginTop: 2 }}
+              >
+                <Typography color="red">{"Don't have an account?"}</Typography>
+                <div
+                  onClick={() => {
+                    setsignUpSwitch(true);
+                    setvalidator(false);
+                    setsucessMessage(false);
+                    setinvalidMessage(false);
+                  }}
+                  style={{ cursor: "pointer", color: "blue" }}
+                >
+                  {"Sign up"}
+                </div>
+              </Stack>
+            </form>
+          </>
+        ) : (
+          <>
+            {/* signup */}
+
+            <form className="login">
+              <Typography color="black" sx={{ fontWeight: "bold" }}>
+                SIGN UP
+              </Typography>
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => {
+                  setinputDatas({ ...inputDatas, email: e.target.value });
+                }}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => {
+                  setinputDatas({ ...inputDatas, password: e.target.value });
+                }}
+              />
+              {validator && (
+                <Typography color="red">All fields are mandatory*</Typography>
+              )}
+              {sucessMessage && (
+                <Typography color="green">
+                  We have sent a link to your email!
+                </Typography>
+              )}
+              {invalidMessage && (
+                <Typography color="red">Give Strong Password!</Typography>
+              )}
+              <button
+                style={{ background: "black" }}
+                onClick={(e) => {
+                  handleSubmitSignup(e);
+                  e.preventDefault();
+                }}
+              >
+                Sign Up
+              </button>
+              <Stack
+                direction="row"
+                spacing={1}
+                alignItems="center"
+                sx={{ marginTop: 2 }}
+              >
+                <Typography color="red">
+                  {"Already have an account?"}
+                </Typography>
+                <div
+                  onClick={() => {
+                    setsignUpSwitch(false);
+                    setvalidator(false);
+                    setsucessMessage(false);
+                    setinvalidMessage(false);
+                  }}
+                  style={{ cursor: "pointer", color: "blue" }}
+                >
+                  {"Log in"}
+                </div>
+              </Stack>
+            </form>
+          </>
+        )}
+      </Container>
+    </>
   );
 }
